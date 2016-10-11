72dda3512b4b2b51c9d8ed94cd9feb1c3de931eb
Maintainer <dl-zar-maintainer@atmel.com>
2016-10-11 14:29:42 +0200

    Primera compilación funcionando Eventos Cliente Web <-> Node <-> Linux. Solamente en Dashboard. Bugs al recibir eventos y estar en otra pagina.

--- public/js/wscli.js
+++ public/js/wscli.js
@@ -1,48 +1,103 @@
-//var socket = io.connect('http://localhost:3000', { 'forceNew': true });
-var socket = io.connect('http://10.140.21.50:3000', { 'forceNew': true });
+//var websocket = io.connect('http://localhost:3000', { 'forceNew': true });
+var websocket = io.connect('http://10.140.21.50:3000', { 'forceNew': true });
 
-socket.on('welcome', function(data) { 
+websocket.on('welcome', function(data) { 
   console.log('Welcome message');
-  console.log(data);
-  render(data);
+  //console.log(data);
+  //render(data);
 })
 
-socket.on('render', function(data) {  
+websocket.on('render', function(data) {  
   console.log('Render new command');
   console.log(data);
-  render(data);
+  //render(data);
 })
 
-function render (data) {  
-  var html = data.map(function(elem, index) {
-    return(`<div>
-              <strong>${elem.user}</strong>:
-              <em>${elem.cmd}</em>
-            </div>`);
-  }).join(" ");
+websocket.on('upddashboard', function(data) {  
+  console.log('Update dash board');
+  console.log(data);
+  updateDashboard(data);
+})
 
-  document.getElementById('messages').innerHTML = html;
+websocket.on('lnxcmd', function(data) {  
+  console.log('Render new lnx command');
+ 
+  var obj = jQuery.parseJSON(JSON.stringify(data));
+  var cmd = obj["0"].lnxcmd;
+  
+  switch (cmd) {
+	  case 64:  // Update Dashboard data (index.html)	  
+	  console.log('update dashboard')
+	  break;
+	  
+	  default:
+	  console.log('lnxcmd not supported')
+  }
+  
+  //render(data);
+})
+
+function updateDashboard(data) {
+	var obj = jQuery.parseJSON(JSON.stringify(data));
+	
+	var x = $('#num_devs').html();
+	console.log(x);
+	console.log(obj["0"].numdevs);
+	$('#num_devs').html(obj["0"].numdevs);
+	$('#num_msgs').html(obj["0"].nummsgs);
+	$('#num_pings').html(obj["0"].numpings);
+	$('#num_paths').html(obj["0"].numpaths);
+	
+	updatePieChartB(obj["0"].net_cov);
+	updatePieChartO(obj["0"].data_cov);
+	updatePieChartT(obj["0"].ping_cov);
+	updatePieChartR(obj["0"].route_errors);
 }
 
-function addMessage(e) {  
-  var message = {
-    user: document.getElementById('user').value,
-    cmd: document.getElementById('command').value
-  };
+// function render (data) {  
+  // var html = data.map(function(elem, index) {
+    // return(`<div>
+              // <strong>${elem.user}</strong>:
+              // <em>${elem.cmd}</em>
+            // </div>`);
+  // }).join(" ");
 
-  console.log('addMessage Form');
-  console.log(message);
-  socket.emit('new-cmd', message);
-  return false;
-}
+  // document.getElementById('messages').innerHTML = html;
+// }
 
-function sendPLCCmd(e) {  
+// function addMessage(e) {  
+  // var message = {
+    // user: document.getElementById('user').value,
+    // cmd: document.getElementById('command').value
+  // };
+
+  // console.log('addMessage Form');
+  // console.log(message);
+  // websocket.emit('new-cmd', message);
+  // return false;
+// }
+
+// function sendPLCCmd(e) {  
+  // var message = {
+    // getpib: document.getElementById('PibReqId').value
+  // };
+  
+  // console.log('Sending message to PLC...');
+  // console.log(message);
+  // websocket.emit('new-plc-cmd', message);
+  // return false;
+// }
+
+function updndev(e) {
+  //var x = $('#num_devs').html();
+  
+  var cmd = 0
+  	
   var message = {
-    getpib: document.getElementById('PibReqId').value
+	webcmd : cmd
   };
   
-  console.log('Sending message to PLC...');
+  console.log('Sending message to PLC...update num devices');
   console.log(message);
-  socket.emit('new-plc-cmd', message);
-  return false;
+  websocket.emit('web-cmd', message);
 }
--- routes/routes.js
+++ routes/routes.js
@@ -2,39 +2,60 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
-// GET home page
-router.get('/', function(req, res) {
-	res.render('index', { title: 'G3' });
-});
-router.get('/index.html', function(req, res) {
-	res.render('index', { title: 'G3' });
-});
+var returnRouter = function(io) {
+	// GET home page
+	router.get('/', function(req, res) {
+		res.render('index', { title: 'G3' });
+	});
+	router.get('/index.html', function(req, res) {
+		res.render('index', { title: 'G3' });
+	});
 
-// Devices page
-router.get('/devices.html', function(req, res) {
-	res.render('devices', { title: 'G3' });
-});
+	// Devices page
+	router.get('/devices.html', function(req, res) {
+		res.render('devices', { title: 'G3' });
+	});
 
-// Network page
-router.get('/network.html', function(req, res) {
-	res.render('network', { title: 'G3' });
-});
+	// Network page
+	router.get('/network.html', function(req, res) {
+		res.render('network', { title: 'G3' });
+	});
 
-// Events page
-router.get('/events.html', function(req, res) {
-	res.render('events', { title: 'G3' });
-});
+	// Events page
+	router.get('/events.html', function(req, res) {
+		res.render('events', { title: 'G3' });
+	});
 
-// Configuration page
-router.get('/configuration.html', function(req, res) {
-	res.render('configuration', { title: 'G3' });
-});
+	// Configuration page
+	router.get('/configuration.html', function(req, res) {
+		res.render('configuration', { title: 'G3' });
+	});
 
-router.get('/cmd', function(req, res) {
-	console.log('Comando Recibido...');
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('Enviando Respuesta.');
-    res.end('Whatever you wish to send \n');
-});
+	// POST REST-API from linux application
+	router.post('/', function(req, res) {
+		console.log('POST Comando Recibido...');
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.write('Enviando Respuesta.');
+		res.end('Whatever you wish to send \n');
+		console.log(req.body);
+		
+		//var dashbrd = require('/home/DCWeb/public/tables/dashboard');
+		var file = fs.readFileSync('/home/DCWeb/public/tables/dashboard.json', 'utf8');
+		var dashbrd = JSON.parse(file);
+		var obj = JSON.parse(JSON.stringify(req.body, null, 2));
+		console.log(obj);
+		console.log(obj["0"].lnxcmd);
+		
+		/* Send Command to WEB client : receive event in wscli.js */
+		if (obj["0"].lnxcmd == 64) {
+			//console.log(dashbrd["0"].net_cov);
+			io.sockets.emit('upddashboard', dashbrd);
+		}
+		
+		
+	});
 
-module.exports = router;
+    return router;
+}
+
+module.exports = returnRouter;
